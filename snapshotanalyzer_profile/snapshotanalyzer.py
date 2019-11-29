dd86176bd4d5fe383bacedbce3868e583c6809f1
krithikap39 <krithikap39@users.noreply.github.com>
2019-11-29 14:13:00 +0530

    Error handling when starting a stopping instance and stopping a pending instance

--- snapshotanalyzer_profile/snapshotanalyzer.py
+++ snapshotanalyzer_profile/snapshotanalyzer.py
@@ -1,4 +1,5 @@
 import boto3
+import botocore
 import click
 
 session = boto3.Session(profile_name='snapshotanalyzer')
@@ -88,7 +89,11 @@ def stop_instances(project):
 
     for i in instances:
         print("Stopping {0}...".format(i.id))
-        i.stop()
+        try:
+            i.stop()
+        except botocore.exceptions.ClientError as e:
+            print("Could not stop {0}  ".format(i.id) + str(e))
+            continue
     return
 
 @instances.command('start')
@@ -101,7 +106,11 @@ def start_instances(project):
 
     for i in instances:
         print("Starting {0}...".format(i.id))
-        i.start()
+        try:
+            i.start()
+        except botocore.exceptions.ClientError as e:
+            print("Could not start {0}  ".format(i.id) + str(e))
+            continue
     return
 
 @instances.command('snapshot')
@@ -129,7 +138,7 @@ def create_snapshots(project):
             i.start()
             i.wait_until_running()
 
-        print("Job completed")        
+    print("Job completed")
     return
 
 if __name__ == '__main__':
