ec9a0ef1fdaea4c3ae29e79965b2d6251291fc64
krithikap39 <krithikap39@users.noreply.github.com>
2019-11-29 13:58:29 +0530

    Wait until instance is stopped before taking snapshot of volume and wait until instance is started again after snapshot creation

--- snapshotanalyzer_profile/snapshotanalyzer.py
+++ snapshotanalyzer_profile/snapshotanalyzer.py
@@ -113,11 +113,23 @@ def create_snapshots(project):
     instances = filter_instances(project)
 
     for i in instances:
+
+        print("Stopping {0}...".format(i.id))
+
         i.stop()
+        i.wait_until_stopped()
+
         for v in i.volumes.all():
             print("Creating snapshots of {0}".format(v.id))
             v.create_snapshot(Description="""Created by snapshotanalyzer
             python script""")
+
+            print("Starting {0}...".format(i.id))
+
+            i.start()
+            i.wait_until_running()
+
+        print("Job completed")        
     return
 
 if __name__ == '__main__':
